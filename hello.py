1bb74d8f5dd7bb0d6ecfc8d7b487e2db4444a987
Rosa Kang <therosessom@gmail.com>
2022-07-04 19:16:48 -0700

    first commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+print('Hello World');
+
+x = 1 + 2 **3 / 4 * 5
+print(x);
+
+# in python3, division forces integer to be float
+print( 10 / 2)
+
+
+nam = input('Who are you? ')
+print('Welcome', nam)  # ',' adds space **
+
+
+inp = input('Europe floor?') #input gives you a string *
+usf = int(inp) + 1
+print('US floor', usf)
