f19a51b155de76c02049162ba071e0930ea15977
Rosa Kang <therosessom@gmail.com>
2022-07-07 07:23:16 -0700

    Day-3

--- 8-data-structures.py
+++ 8-data-structures.py
@@ -1,9 +1,4 @@
-from hashlib import algorithms_available
-from re import T
-from sqlite3 import ProgrammingError
-
-
-ProgrammingError
+# Programming
 # Algorithms : a set of rules or steps used to solve problem ex.loops
 # Data Structure : a particular way of organizing data in a computer ex. string, list etc
 
@@ -12,7 +7,27 @@ ProgrammingError
 # fruit[2] = T #won't work as str doesn't support assignment.
 # TypeError: 'str' object does not support item assignment
 
-fruit = 'Banana'
-x = fruit.lower()
-print(x)
+# fruit = 'Banana'
+# x = fruit.lower()
+# print(x)
 
+# # List Method
+# x = list() #list() is reserved class in python
+# print(type(x))
+# print(dir(x))
+
+# friends = ['Josh', 'Glen', 'Rose']
+# friends.sort()
+# print(friends)
+
+# Average
+total = 0
+count = 0
+while True:
+    inp = input('Enter a number: ')
+    if inp == 'done' : break
+    value = float(inp)
+    total = total + value
+    count = count + 1
+average = total / count
+print('Average: ', average)
